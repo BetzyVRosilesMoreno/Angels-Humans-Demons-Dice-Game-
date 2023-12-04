22c1f1af409585dfb59a4fdb4482aacd4f9f8462
Betzy V. Rosiles Moreno <brosilesmoreno@bsd220.org>
2023-12-04 09:25:21 -0600

    Initial Commit

---
+++ Angels, Humans, Demons (Dice Game)/Angels__Humans__Demons__Dice_Game_App.swift
@@ -0,0 +1,17 @@
+//
+//  Angels__Humans__Demons__Dice_Game_App.swift
+//  Angels, Humans, Demons (Dice Game)
+//
+//  Created by Betzy Moreno on 12/4/23.
+//
+
+import SwiftUI
+
+@main
+struct Angels__Humans__Demons__Dice_Game_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Angels, Humans, Demons (Dice Game)/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Angels, Humans, Demons (Dice Game)
+//
+//  Created by Betzy Moreno on 12/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
