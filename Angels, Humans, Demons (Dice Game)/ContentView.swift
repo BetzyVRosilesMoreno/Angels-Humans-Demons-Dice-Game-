e351d49ae0874b21f27a2ab29fb30bc3d374eb3d
Betzy V. Rosiles Moreno <brosilesmoreno@bsd220.org>
2023-12-11 10:00:25 -0600

    "Added Dice image view"

--- Angels, Humans, Demons (Dice Game)/ContentView.swift
+++ Angels, Humans, Demons (Dice Game)/ContentView.swift
@@ -10,18 +10,42 @@ import SwiftUI
 struct ContentView: View {
     @State private var playerOne = 0
     @State private var playerTwo = 0
+    @State private var randomeValue = 0
+    @State private var rotation = 0.0
     var body: some View {
         ZStack {
             Color.brown.opacity(0.7).ignoresSafeArea()
         VStack {
             Image("Logo").resizable().frame(width: 260, height: 260)
             CustomText(text: "Angels, Humans, Demons")
-                .font(.custom("block_out", size: 16))
+            HStack {
+                Image("Demon Dice \(randomeValue)")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .rotationEffect(.degrees(rotation))
+                    .rotation3DEffect(.degrees(rotation), axis: (x: 1, y: 1, z: 0))
+                    .padding()
+                
+                Image("pips \(randomeValue)")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .rotationEffect(.degrees(rotation))
+                    .rotation3DEffect(.degrees(rotation), axis: (x: 1, y: 1, z: 0))
+                    .padding()
+                
+                Image("Angel Dice \(randomeValue)")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .rotationEffect(.degrees(rotation))
+                    .rotation3DEffect(.degrees(rotation), axis: (x: 1, y: 1, z: 0))
+                    .padding()
+            }
             Spacer()
             CustomText(text: "player One: \(playerOne)")
             CustomText(text: "player Two: \(playerTwo)")
             Spacer()
         }
+        .font(.custom("block_out", size: 26))
       }
     }
 }
