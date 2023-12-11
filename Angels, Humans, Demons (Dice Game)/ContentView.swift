362e893c0550aedb524c56f9632a2e9128361221
Betzy V. Rosiles Moreno <brosilesmoreno@bsd220.org>
2023-12-11 09:30:03 -0600

    "Added player One and Two"

--- Angels, Humans, Demons (Dice Game)/ContentView.swift
+++ Angels, Humans, Demons (Dice Game)/ContentView.swift
@@ -8,18 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var playerOne = 0
+    @State private var playerTwo = 0
     var body: some View {
         ZStack {
             Color.brown.opacity(0.7).ignoresSafeArea()
         VStack {
             Image("Logo").resizable().frame(width: 260, height: 260)
-            Text("Angels, Humans, Demons")
+            CustomText(text: "Angels, Humans, Demons")
+                .font(.custom("block_out", size: 16))
+            Spacer()
+            CustomText(text: "player One: \(playerOne)")
+            CustomText(text: "player Two: \(playerTwo)")
             Spacer()
         }
       }
     }
 }
-
+struct CustomText: View {
+    let text: String
+    var body: some View {
+        Text(text).font(Font.custom("block_out", size: 36))
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
